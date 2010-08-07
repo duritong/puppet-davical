b93c3ed6ac5de922d61e05cc4b01ae397491969e
mh <mh@immerda.ch>
2010-08-07 02:12:14 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,7 @@
 define davical::config(){
   file{"/etc/davical/${name}-conf.php":
-    source => [ "puppet://$server/modules/site-davical/conf/${fqdn}/${name}-conf.php",
-                "puppet://$server/modules/site-davical/conf/${name}-conf.php" ],
+    source => [ "puppet:///modules/site-davical/conf/${fqdn}/${name}-conf.php",
+                "puppet:///modules/site-davical/conf/${name}-conf.php" ],
     require => Package['davical'],
     owner => root, group => apache, mode => 0640;
   }
