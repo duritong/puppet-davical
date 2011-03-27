bd54582b3dafc8ae162f2363d99bddb664e3ad81
mh <mh@immerda.ch>
2011-03-27 16:44:17 +0200

    deploy administration file

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,4 +3,11 @@ class davical::base {
     ensure => present,
   }
   include perl::extensions::yaml
+  
+  file{'/etc/davical/administration.yml':
+    source => [ "puppet:///modules/site-davical/conf/${fqdn}/administration.yml",
+                "puppet:///modules/site-davical/conf/administration.yml" ],
+    require => Package['davical'],
+    owner => root, group => root, mode => 0640;
+  }
 }
