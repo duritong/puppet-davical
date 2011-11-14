8639dc671b6e501b0c04836234c36da40c400502
mh <mh@immerda.ch>
2011-11-15 00:47:33 +0100

    introduce fcgid for davical

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -4,7 +4,10 @@ define davical::vhost(
   $ssl_mode = 'force',
   $logmode = 'default',
   $mod_security = false,
-  $monitor_url = 'absent'
+  $monitor_url = 'absent',
+  $run_mode = 'normal',
+  $run_uid = 'absent',
+  $run_gid = 'absent'
 ){
   include ::davical
   file{"/etc/davical/${name}-conf.php":
@@ -12,6 +15,22 @@ define davical::vhost(
                 "puppet:///modules/site-davical/conf/${name}-conf.php" ],
     require => Package['davical'],
     owner => root, group => apache, mode => 0640;
+  }
+
+  if ($run_mode == 'fcgid'){
+    if (($run_uid == 'absent') or ($run_gid == 'absent')) { fail("Need to configure \$run_uid and \$run_gid if you want to run Phpmyadmin::Vhost[${name}] as fcgid.") }
+
+    user::managed{$name:
+      ensure => $ensure,
+      uid => $run_uid,
+      gid => $run_gid,
+      shell => $operatingsystem ? {
+        debian => '/usr/sbin/nologin',
+        ubuntu => '/usr/sbin/nologin',
+        default => '/sbin/nologin'
+      },
+      before => Apache::Vhost::Php::Standard[$name],
+    }
   }
 
   apache::vhost::php::standard{$name:
@@ -23,16 +42,28 @@ define davical::vhost(
       gentoo => '/var/log/apache2',
       default => '/var/log/httpd'
     },
+    run_mode => $run_mode,
+    run_uid => $name,
+    run_gid => $name,
     logmode => $logmode,
     manage_webdir => false,
     path_is_webdir => true,
     ssl_mode => $ssl_mode,
+    php_settings => {
+      safe_mode         => 'Off',
+      include_path      => '/usr/share/awl/inc',
+      magic_quotes_gpc  => 0,
+      register_globals  => 'On',
+      open_basedir      => 'On',
+      error_reporting   => "E_ALL & ~E_NOTICE",
+      default_charset   => "utf-8"
+    },
     mod_security => $mod_security,
     template_partial => 'davical/vhost/php_stuff.erb',
     require => Package['davical'],
   }
 
-  if $use_nagios {
+  if hiera('use_nagios',false) {
     $real_monitor_url = $monitor_url ? {
       'absent' => $name,
       default => $monitor_url,
